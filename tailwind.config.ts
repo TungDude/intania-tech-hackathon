8509f005cf902ee20c522b3865fbfe6bfcf47c12
mildrrnt <woranita888@gmail.com>
2025-03-29 23:02:57 +0700

    feat: scene 5

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,18 +4,6 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-<<<<<<< HEAD
-        'zoom-in-door-0': 'zoom-in 4s ease-in-out forwards',
-        'fade-in-door-0': 'fade-in 4s ease-in-out forwards',
-        'fade-text-0': 'fade-in-out 6s ease-in-out forwards',
-      },
-      keyframes: {
-        'zoom-in': {
-          '0%': { transform: 'scale(0.1)', opacity: '0'},
-          '50%': { opacity: '1' },
-          // '80%': { },
-          '100%': { transform: 'scale(8)', opacity: '0' }
-=======
         "zoom-in": "zoom-in 0.5s ease-in-out forwards", // Define the animation name, duration, and easing
         "bounce-subtle": "bounce-subtle 0.5s ease-in-out infinite",
       },
@@ -27,7 +15,6 @@ module.exports = {
         "bounce-subtle": {
           "0%, 100%": { transform: "translateY(0)" },
           "50%": { transform: "translateY(-5px)" },
->>>>>>> 0eca465c7c568252f5501ebb6fe9eb53eb04986f
         },
         'fade-in': {
           '0%': { opacity: '0' },
